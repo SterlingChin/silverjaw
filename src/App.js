243e01cfd171a6b71d9da53ee2e3d762fe1e701e
Sterling Chin <schin@overstock.com>
2018-09-18 08:19:12 -0600

    update styling

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import './App.css';
 import Header from './Header/Header';
 import Silverjaw from './Silverjaw/Silverjaw';
 import IAmFed from './IAmFed/IAmFed';
+import { isMobile } from './utils/Browsers';
 
 class App extends Component {
   render() {
+    console.log(isMobile);
+    if (isMobile) {
+      return (
+        <div className="App">
+            <Header />
+            <IAmFed />
+            <Silverjaw />
+        </div>
+      )
+    }
     return (
       <div className="App">
         <Header />
---
+++ src/utils/Browsers.js
@@ -0,0 +1,1 @@
+module.exports = { isMobile: window.innerWidth < 768 && navigator.maxTouchPoints > 0 };
