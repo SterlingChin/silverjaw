0dbc4d2608096350f3c5998d7c6d23372c405198
Sterling Chin <schin@overstock.com>
2018-12-18 23:28:08 -0700

    add about

--- src/About/About.js
+++ src/About/About.js
@@ -8,7 +8,6 @@ const About = () => {
         line1: 'I am a front end developer at Overstock.com, and a husband and father to 2 amazing boys.',
         line2: 'I love Javascript, plain and simple. I try to be framework agnostic, but I lean toward React w/ Redux. When I\'m not coding, I\'m hiking, riding my motorcycle, flying drones, attending Real Salt Lake soccer games, or playing videos games.',
         line3: 'As dictated by my son: Daddy\'s so sweet! And Daddy plays with Taylor and Grayson.  And we play in the sandbox.  And Grayson and Taylor play in the toybox.  And Daddy needs to take me to Lulu\'s House, and soccer games, and school. And Daddy needs to play with me with toys tomorrow.  And play ball with our pet.  And read books. And we want to pick blueberries to make blueberry punch.',
-        line3: 'Hi Andrew, how are you doing?',
     }
     if (isMobile) {
         return (
