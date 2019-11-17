01404419dba7f3047405ee9c8fd127540a208199
Sterling <sterlingchin@workfront.com>
2019-11-17 16:57:24 -0700

    move Silverjaw to styled component

--- src/Silverjaw/Silverjaw.js
+++ src/Silverjaw/Silverjaw.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { isMobile } from '../utils/Browsers';
-import './Silverjaw.css';
 
 const SJLogo = styled('img')`
     height: ${({isMobile}) => isMobile ? '150px' :'350px'};
+    width: ${({isMobile}) => isMobile ? '95%' :'auto'};
+    margin: 25px;
 `
 SJLogo.displayName = 'SJLogo'
+
 
 const Silverjaw = () => (
     <SJLogo isMobile={isMobile} src={require('../Images/silverjaw.png')} alt='silverjaw logo'/>
