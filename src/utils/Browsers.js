b85592ec6f5492878317ac9a6000965aa2ef7eea
Sterling <sterlingchin@workfront.com>
2019-11-17 16:02:13 -0700

    add comment in Browers.js

--- src/utils/Browsers.js
+++ src/utils/Browsers.js
@@ -1,1 +1,1 @@
-module.exports = { isMobile: window.innerWidth <= 768 && navigator.maxTouchPoints > 0 };
+module.exports = { isMobile: window.innerWidth <= 768 && navigator.maxTouchPoints > 0 }; //This checks if a.) the innerWidth is tablet size or smaller, and b.) if the device is touch screen
