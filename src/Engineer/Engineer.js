3b50975d6320ccedf78ebaf2a59cb9a71b673da4
Sterling <sterlingchin@workfront.com>
2019-11-17 17:48:30 -0700

    move silverjaw component to the engineer component

--- src/Engineer/Engineer.js
+++ src/Engineer/Engineer.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import Silverjaw from '../Silverjaw/Silverjaw';
 import { isMobile } from '../utils/Browsers';
 
 const EngineerContainer = styled('div')`
+    display: flex;
+    filter: drop-shadow(0 1px 2px rgba(0, 35, 64, 0.32));
+`
+EngineerContainer.displayName = 'EngineerContainer'
+
+const Title = styled('div')`
     font-size: ${({isMobile}) => isMobile ? '1.5em' :'2em'};
     display: inline-block;
     vertical-align: middle;
     height: 50px;
+    line-height: 50px;
     align-self: center;
     margin: ${({isMobile}) => isMobile ? '15px auto' :'0 0 100px 0'};
+    filter: drop-shadow(0 1px 2px rgba(0, 35, 64, 0.32));
 `
-EngineerContainer.displayName = 'EngineerContainer'
+Title.displayName = 'Title'
+
 
 
 const SoftwareEngineer = () => (
     <EngineerContainer isMobile={isMobile}>
-        Software Engineer
+        <Silverjaw />
+        <Title>Software Engineer</Title>
     </EngineerContainer>
 );
 
