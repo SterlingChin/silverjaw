657381892fbd5bfebe47e5148aa680efb8af0635
Sterling <sterlingchin@workfront.com>
2019-11-18 10:33:10 -0700

    move comment above module.exports in Browsers.js

--- src/utils/Browsers.js
+++ src/utils/Browsers.js
@@ -1,1 +1,2 @@
-module.exports = { isMobile: window.innerWidth <= 768 && navigator.maxTouchPoints > 0 }; //This checks if a.) the innerWidth is tablet size or smaller, and b.) if the device is touch screen
+//This checks if a.) the innerWidth is tablet size or smaller, and b.) if the device is touch screen
+module.exports = { isMobile: window.innerWidth <= 768 && navigator.maxTouchPoints > 0 };
