ea21a61d8bcc1730cf0a6b33c849d64d4ef6f444
Sterling <sterlingchin@workfront.com>
2019-11-17 19:57:54 -0700

    update footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import './Footer.css';
+import styled from '@emotion/styled'
+
+const FooterContainer = styled('div')`
+    height: 50px;
+    width: 100%;
+    text-align: center;
+    position: fixed;
+    bottom: 0;
+`
+FooterContainer.displayName = 'FooterContainer'
+
 
 const Footer = () => {
     return (
-        <div className="footer-body">
-            Copyright 2018.
-        </div>
+        <FooterContainer className="footer-body">
+            Copyright 2019.
+        </FooterContainer>
     )
 };
 
