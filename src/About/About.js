cda1dcdbe377d83918e6ce3d965b3524f912e9b3
Sterling <sterlingchin@workfront.com>
2019-11-17 17:47:44 -0700

    add new messages to About, update css, add filter

--- src/About/About.js
+++ src/About/About.js
@@ -5,10 +5,14 @@ import { isMobile } from '../utils/Browsers';
 
 const messages = [
     'Software Engineer with 3 years experience currently at Workfront',
-    'Tech Stack: React w/ Hooks & Redux, Javascript, Amazon AWS, Gitlab pipelines, Styled Components (Emotion)'
+    'Tech Stack: React w/ Hooks & Redux, Javascript, Jest & Enzyme, Amazon AWS, Gitlab pipelines, and styled components (Emotion)',
+    'Professional Goals: Mentor new engineers, write clean & testable code (I strive for 100% test coverage), and continue to level up my craft',
+    'Professional Interests: TDD (Test Driven Developement), React.js, Vue.js, pixel match mocks, and speaking at conferences',
+    'Interests: Snowboarding, Hiking, Motorcycles, Tattoos, Space, Mechanical Keyboards, and of course my amazing family'
 ];
 const links = {
     'Resume': 'https://drive.google.com/file/d/1zzO7s2kteazY5E3R3Xzo_5a8G7E_0--6/view?usp=sharing',
+    'Shakespear Review': 'https://sterling-chin-podium-challenge-home.now.sh/',
     'DevMountain promo': 'https://www.youtube.com/watch?v=QlaOlvteRec',
     'Christmas Tree (pure HTML/CSS)': 'https://christmastree.now.sh',
 };
@@ -18,7 +22,7 @@ const AboutContainer = styled('div')`
     display: inline-block;
     vertical-align: middle;
     align-self: center;
-    margin: ${({isMobile}) => isMobile ? '15px auto' :'0 0 100px 0'};
+    margin: ${({isMobile}) => isMobile ? '15px auto' :'0px'};
     text-align: left;
 `
 AboutContainer.displayName = 'AboutContainer';
@@ -27,6 +31,7 @@ const SectionTitle = styled('div')`
     font-size: 24px;
     width: 100%;
     margin: ${({isMobile}) => isMobile ? '5px' :'0px'};
+    filter: drop-shadow(0 1px 2px rgba(0, 35, 64, 0.32));
 `
 SectionTitle.displayName = 'SectionTitle';
 
@@ -35,7 +40,8 @@ const Message = styled('div')`
     line-height: 18px;
     width: 100%;
     padding: 5px;
-    margin: 0 0 5px 0;
+    margin: 0 0 8px 0;
+    filter: drop-shadow(0 1px 2px rgba(0, 35, 64, 0.32));
     a {
         color: ${COLORS.LINK};
         text-decoration: none;
